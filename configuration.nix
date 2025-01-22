f95783e20bfecfb4f994824f5f8b6ba4fad27347
frerepu <frederic@republiekbrugge.be>
2025-01-22 15:56:35 +0100

    deleted vscode from configuration because in home-manager

--- configuration.nix
+++ configuration.nix
@@ -94,24 +94,6 @@
     wget
     curl
     tailscale 
-    # vscode
-    (vscode-with-extensions.override {
-    vscodeExtensions = with vscode-extensions; [
-      bbenoist.nix
-      esbenp.prettier-vscode
-      ms-python.python
-      tailscale.vscode-tailscale
-      ms-azuretools.vscode-docker
-      ms-vscode-remote.remote-ssh
-    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "remote-ssh-edit";
-        publisher = "ms-vscode-remote";
-        version = "0.47.2";
-        sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-      }
-    ];
-  })
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
