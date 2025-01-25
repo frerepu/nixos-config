5617fc09af0d524a18e41fd3c23847fc4884e870
frerepu <frederic@republiekbrugge.be>
2025-01-25 10:56:06 +0100

    added catppuccin

--- flake.nix
+++ flake.nix
@@ -1,18 +1,19 @@
 {
     description = "My first flake";
 
-    
+
     inputs = {
       nixpkgs.url = "nixpkgs/nixos-24.11";
       home-manager = {
         url = "github:nix-community/home-manager/release-24.11";
         inputs.nixpkgs.follows = "nixpkgs";
       };
+      catppuccin.url = "github:catppuccin/nix";
       zen-browser.url = "github:0xc000022070/zen-browser-flake";
     };
 
 
-    outputs = { self, nixpkgs, home-manager, zen-browser,... }: 
+    outputs = { self, nixpkgs, home-manager, zen-browser, catppuccin,... }:
       let
         lib = nixpkgs.lib;
         system = "x86_64-linux";
@@ -31,9 +32,12 @@
           inherit pkgs;
           modules = [
             ./home.nix
-            {nixpkgs.config.allowUnfree = true;
-            # Add zen-browser module directly here
-             home.packages = [ zen-browser.packages.${system}.default ]; }
+            catppuccin.homeManagerModules.catppuccin
+            {
+              nixpkgs.config.allowUnfree = true;
+              # Add zen-browser module directly here
+              home.packages = [ zen-browser.packages.${system}.default ];
+            }
           ];
         };
       };
