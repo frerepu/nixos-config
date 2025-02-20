727277324c59bb87d3e57a2a31296f7ea4771dd5
frerepu <frederic@republiekbrugge.be>
2025-02-20 20:26:31 +0100

    cleanup

--- configuration.nix
+++ configuration.nix
@@ -2,9 +2,7 @@
 
 {
   imports = [ 
-    #./hardware-configuration.nix  # Keep using the old location temporarily
     ./hosts/common/default.nix
     ./hosts/desktop/default.nix
-    #./hosts/desktop/hardware.nix
   ];
 }
