1740f76c86201d8d5d3d1397b39f1b0a8bf8a08c
frerepu <frederic@republiekbrugge.be>
2025-02-23 21:07:42 +0100

    cleanup

--- home.nix
+++ home.nix
@@ -180,16 +180,6 @@ in
       size = "standard";
       tweaks = ["normal"];
   };
-  
-
-  # xdg.desktopEntries.yazi = {
-  #   name = "Yazi";
-  #   comment = "Terminal file manager";
-  #   terminal = false;  # Changed this to false
-  #   exec = "kitty -e yazi";
-  #   categories = [ "System" "FileManager" ];
-  #   type = "Application";
-  # };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
