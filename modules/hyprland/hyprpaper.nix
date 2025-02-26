1c15c1a56ee864492fe9c66d90afb3de108c206e
frerepu <frederic@republiekbrugge.be>
2025-02-26 21:53:28 +0100

    adding hyprpaper

---
+++ modules/hyprland/hyprpaper.nix
@@ -0,0 +1,14 @@
+{
+  services.hyprpaper = {
+    enable = true;
+    settings= [
+        {
+            monitor = "";
+            path = "$HOME/.config/background/wallhaven-8x16mo.png";
+            blur_passes = 2;
+            color = "rgb(30, 30, 46)";
+        }
+    ];
+
+  };
+}
