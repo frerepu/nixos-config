77770e4dbb09c37c7bc051aed3384033ac8bb570
frerepu <frederic@republiekbrugge.be>
2025-02-26 22:04:24 +0100

    added hyprpaper settings

--- modules/hyprland/hyprpaper.nix
+++ modules/hyprland/hyprpaper.nix
@@ -1,14 +1,15 @@
 {
   services.hyprpaper = {
     enable = true;
-    settings= [
-        {
-            monitor = "";
-            path = "$HOME/.config/background/wallhaven-8x16mo.png";
-            blur_passes = 2;
-            color = "rgb(30, 30, 46)";
-        }
-    ];
+    settings= {
+      preload = [
+        "/home/faelterman/.config/background/wallhaven-8x16mo.png"
+      ];  
+      wallpaper = [
+        "DP-1:$HOME/.config/background/wallhaven-8x16mo.png"
+      ];
+    }; 
+        
 
   };
 }
