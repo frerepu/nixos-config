b7d73e89bf5cdfe692ac6cac0be4f7d88ee8c21b
frerepu <frederic@republiekbrugge.be>
2025-03-07 21:00:24 +0100

    added figlet.nix (whitout ansi shadow font config)

--- modules/figlet.nix
+++ modules/figlet.nix
@@ -1,27 +1,42 @@
-{ config, lib, pkgs, ... }:
+# ███████╗██╗ ██████╗ ██╗     ███████╗████████╗
+# ██╔════╝██║██╔════╝ ██║     ██╔════╝╚══██╔══╝
+# █████╗  ██║██║  ███╗██║     █████╗     ██║   
+# ██╔══╝  ██║██║   ██║██║     ██╔══╝     ██║   
+# ██║     ██║╚██████╔╝███████╗███████╗   ██║   
+# ╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚══════╝   ╚═╝  
 
-let
-  ansiShadowFont = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/xero/figlet-fonts/master/ansi_shadow.flf";
-    sha256 = "sha256-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx="; # Replace with the actual sha256 hash
-  };
-in
+{ config, lib, pkgs, ... }:
 {
-  options = {
-    services.figlet = {
-      enable = lib.mkOption {
-        type = lib.types.bool;
-        default = false;
-        description = "Enable figlet service and install fonts.";
-      };
+  options.services.figlet = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable figlet service.";
     };
   };
-
+  
   config = lib.mkIf config.services.figlet.enable {
-    environment.systemPackages = with pkgs; [
+    # Simply install figlet
+    home.packages = with pkgs; [
       figlet
     ];
-
-    environment.etc."figlet/fonts/ansi_shadow.flf".source = ansiShadowFont;
+    
+    # Create a simple script that allows you to use the ANSI Shadow font
+    home.file.".local/bin/ansi-shadow" = {
+      executable = true;
+      text = ''
+        #!/usr/bin/env bash
+        ${pkgs.figlet}/bin/figlet -f "$(dirname "$(dirname "$(which figlet)")")/share/figlet/fonts/shadow.flf" "$@"
+      '';
+    };
+    
+    # Make sure .local/bin exists and is in PATH
+    home.activation.createLocalBin = lib.hm.dag.entryBefore ["writeBoundary"] ''
+      $DRY_RUN_CMD mkdir -p $VERBOSE_ARG ~/.local/bin
+    '';
+    
+    home.sessionPath = [
+      "$HOME/.local/bin"
+    ];
   };
 }
