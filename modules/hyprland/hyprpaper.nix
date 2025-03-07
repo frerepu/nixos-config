19b20ac92d278760336b90fc04af4e44f6b0287a
frerepu <frederic@republiekbrugge.be>
2025-03-07 21:53:28 +0100

    fixed issue with badly configured display name / added ascii

--- modules/hyprland/hyprpaper.nix
+++ modules/hyprland/hyprpaper.nix
@@ -1,15 +1,18 @@
+# ██╗  ██╗██╗   ██╗██████╗ ██████╗ ██████╗  █████╗ ██████╗ ███████╗██████╗ 
+# ██║  ██║╚██╗ ██╔╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗
+# ███████║ ╚████╔╝ ██████╔╝██████╔╝██████╔╝███████║██████╔╝█████╗  ██████╔╝
+# ██╔══██║  ╚██╔╝  ██╔═══╝ ██╔══██╗██╔═══╝ ██╔══██║██╔═══╝ ██╔══╝  ██╔══██╗
+# ██║  ██║   ██║   ██║     ██║  ██║██║     ██║  ██║██║     ███████╗██║  ██║
+# ╚═╝  ╚═╝   ╚═╝   ╚═╝     ╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝
+{config, pkgs, lib, ...}:
 {
   services.hyprpaper = {
     enable = true;
     settings= {
-      preload = [
-        "/home/faelterman/.config/background/wallhaven-8x16mo.png"
-      ];
-      wallpaper = [
-        "DP-1:$HOME/.config/background/wallhaven-8x16mo.png"
-      ];
+    	ipc = "on";
+	    splash =  false;
+      preload = ["/home/faelterman/.dotfiles/modules/hyprland/wp.jpg"];
+      wallpaper = ["eDP-1, /home/faelterman/.dotfiles/modules/hyprland/wp.jpg"];
     };
-
-
   };
 }
