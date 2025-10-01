2e08fc5298bf4550ec3448a0525e6051bd473f9b
frerepu <frederic@republiekbrugge.be>
2025-10-01 16:40:48 +0200

    getting tailscal working again

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -55,12 +55,11 @@
     openFirewall = true;
     authKeyFile = "/home/faelterman/secrets/tsauthkeyfile";
     useRoutingFeatures = "both";
-    interfaceName = "userspace-networking";
+    #interfaceName = "userspace-networking";
     extraUpFlags = [
-      "--advertise-exit-node" # if you want to use it as an exit node 
-      "--advertise-routes=10.0.0.0/24,172.16.10.0/24" # if you want to advertise routes 
-      # "--login-server=https://your-instance" # if you use a non-default tailscale coordinator
-      # "--accept-dns=false" # if its' a server you prolly dont need magicdns
+      "--advertise-exit-node"
+      "--advertise-routes=10.0.0.0/24,172.16.10.0/24"
+      "--accept-routes"  # Add this line
     ];
   };
   
