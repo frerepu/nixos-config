4075036a8f4aaea73edbf1e103c0c3b54199f65e
frerepu <frederic@republiekbrugge.be>
2025-10-19 22:47:01 +0200

    cleaned up aliases from home.nix into zsh.nix

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -4,10 +4,21 @@
 #  ███╔╝  ╚════██║██╔══██║
 # ███████╗███████║██║  ██║
 # ╚══════╝╚══════╝╚═╝  ╚═╝
+{ config, pkgs, ... }:
 {
   programs.zsh = {
     enable = true;
-    shellAliases = myAliases;
+    shellAliases = {
+      ll = "ls -l";
+      ".." = "cd ..";
+      c = "clear";
+      ls = "ls -lah --color=auto";
+      cat = "bat";
+      figletas = "figlet -f ~/.local/share/figlet/fonts/ansi_shadow.flf";
+    };
+    initExtra = ''
+      eval "$(starship init zsh)"
+    '';
     envExtra = ''
       # Suppress Starship error messages in non-interactive sessions
       if [[ "$TERM" == "dumb" ]] || [[ ! -t 0 && ! -t 1 ]]; then
